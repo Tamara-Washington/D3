bdb3de6d20e08c63f47367b4a1c314e25c959fe3
tamara washington <tamey.washington@gmail.com>
2021-03-16 00:47:54 -0500

    svg area added

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,5 +1,24 @@
-// @TODO: YOUR CODE HERE!
+//Create SVG area
+let svgWidth = 800;
+let svgHeight = 500;
 
+//Set margins
+let margin = {
+    top:60,
+    right:60,
+    bottom:60,
+    left:60
+};
+
+//Create chart area
+let chartWidth = svgWidth - margin.left - margin.right;
+let chartHeight = svgHeight - margin.top - margin.bottom;
+
+//Add the SVG tag
+let svg = d3.select('#scatter')
+    .append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
 
 //Read in CSV
 d3.csv('assets/data/data.csv').then(function (csvInfo) {
