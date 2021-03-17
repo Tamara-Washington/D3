cbd7bc6bed6fb1b5ad83a08feaf90f3f40d44396
tamara washington <tamey.washington@gmail.com>
2021-03-16 20:40:07 -0500

    Appended axes

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -20,6 +20,12 @@ let svg = d3.select('#scatter')
     .attr('width', svgWidth)
     .attr('height', svgHeight);
 
+//Create the chartGroup
+let chartGroup = svg.append('g')
+    .attr('transform', `translate (${margin.left}, ${margin.top})`);
+
+//Circle
+
 //Read in CSV
 d3.csv('assets/data/data.csv').then(function (csvInfo) {
     console.log(csvInfo);
@@ -35,7 +41,26 @@ d3.csv('assets/data/data.csv').then(function (csvInfo) {
         .domain([0, d3.max(csvInfo.map (record => record.smokes))])
         .range([svgHeight, 0]);
 
-    let XScale = d3.scaleLinear()
+    let xScale = d3.scaleLinear()
         .domain([0, d3.max(csvInfo.map (record => record.age))])
         .range([0, svgWidth]);
+
+    //Create axes functions
+    let axisBottom = d3.axisBottom(xScale);
+    let axisLeft = d3.axisLeft(yScale);
+
+    //Create axes and pass scales 
+    let xAxis = d3.axisBottom(xScale);
+        yAxis = d3.axisLeft(yScale);
+
+    //Put axes on chart
+    chartGroup.append('g')
+        .attr('transform', `translate(0, ${chartHeight})`)
+        .attr(axisBottom);
+
+    chartGroup.append('g')
+        .call(axisLeft)
+
+    //Add circles
 });
+
